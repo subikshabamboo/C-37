4f19536bc09f6f6fca191db2198f74b04cc79556
subikshabamboo <61645416+subikshabamboo@users.noreply.github.com>
2021-03-02 18:53:08 +0530

    Add files via upload

---
+++ asynchronousBallMovement-master/sketch.js
@@ -0,0 +1,26 @@
+var database,form,player,game;
+var gameState = 0;
+var playerCount;
+var allPlayers;
+
+var car1,car2,car3,car4,cars;
+
+function setup(){
+    database=firebase.database()
+    createCanvas(displayWidth,displayHeight);
+    game = new Game();
+    game.getState();
+    game.start();
+
+}
+
+function draw(){
+    background("white");
+    if(playerCount === 4){
+        game.update(1);
+    }
+    if(gameState === 1){
+        clear();
+        game.play();
+    }
+}
